fc5b4b623cb98a2aabdcd6adbd06b4ecd36288ce
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-13 18:57:00 +0000

    [BC-001] Execute Identity scaffolding and connect to DB
    Using Secret Manager to store DB Connection secrets.

---
+++ App/BucksCalendar/BucksCalendar/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+using System;
+using BucksCalendar.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BucksCalendar.Areas.Identity.IdentityHostingStartup))]
+namespace BucksCalendar.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        private string _connectionString = null;
+        
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                var connectionStringBuilder = new SqlConnectionStringBuilder(context.Configuration.GetConnectionString("BucksCalendarIdentityDbContextConnection"));
+                connectionStringBuilder.Password = context.Configuration["CalendarDbPassword"];
+                _connectionString = connectionStringBuilder.ConnectionString;
+                
+                services.AddDbContext<BucksCalendarIdentityDbContext>(options =>
+                    options.UseSqlServer(_connectionString));
+
+                services.AddDefaultIdentity<CalendarUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BucksCalendarIdentityDbContext>();
+            });
+        }
+    }
+}
