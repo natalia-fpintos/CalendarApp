e6b43c8f0870a418c8d9726a60a6a10b5daaa0e9
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-29 23:32:29 +0100

    [BC-006] Change create event form fields and model

--- App/BucksCalendar/BucksCalendar/Pages/Calendar/Create.cshtml.cs
+++ App/BucksCalendar/BucksCalendar/Pages/Calendar/Create.cshtml.cs
@@ -1,23 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using BucksCalendar.Areas.Identity.Data;
+using BucksCalendar.Areas.Identity.Validations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using BucksCalendar.Data;
 using BucksCalendar.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace BucksCalendar.Pages.Calendar
 {
     public class CreateModel : PageModel
     {
-        private readonly BucksCalendar.Data.DatabaseContext _context;
+        private readonly DatabaseContext _context;
+        private readonly UserManager<CalendarUser> _userManager;
 
-        public CreateModel(BucksCalendar.Data.DatabaseContext context)
+        public CreateModel(DatabaseContext context, UserManager<CalendarUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
+        }
+        
+        [BindProperty]
+        public EventInput Input { get; set; }
+        
+        public class EventInput
+        {
+            [Required(ErrorMessage = "This field is required.")]
+            [DataType(DataType.Text)]
+            [Display(Name = "Category")]
+            public int CategoryID { get; set; }
+            
+            [Required(ErrorMessage = "This field is required.")]
+            [DataType(DataType.Text)]
+            public string Title { get; set; }
+            
+            [DataType(DataType.Text)]
+            public string Description { get; set; }
+            
+            [Display(Name = "All day event")]
+            public bool AllDayEvent { get; set; }
+            
+            [Required(ErrorMessage = "This field is required.")]
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Start date")]
+            public DateTime StartDateTime { get; set; }
+
+            [Required(ErrorMessage = "This field is required.")]
+            [DataType(DataType.DateTime)]
+            [Display(Name = "End date")]
+            public DateTime EndDateTime { get; set; }
+            
+            [Display(Name = "Notify by SMS")]
+            public bool NotifyBySMS { get; set; }
+            
+            [Display(Name = "Notify by Email")]
+            public bool NotifyByEmail { get; set; }
+            
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Scheduled for")]
+            public DateTime ScheduledFor { get; set; }
         }
 
         public IActionResult OnGet()
@@ -25,20 +71,39 @@ namespace BucksCalendar.Pages.Calendar
         ViewData["UserID"] = new SelectList(_context.Set<CalendarUser>(), "Id", "Id");
             return Page();
         }
-
-        [BindProperty]
-        public Event Event { get; set; }
-
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
+        
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+            
+            var calendarEvent = new Event { 
+                UserID = _userManager.GetUserId(User),
+                CategoryID = Input.CategoryID,
+                AllDayEvent = Input.AllDayEvent,
+                StartDateTime = Input.StartDateTime, 
+                EndDateTime = Input.EndDateTime, 
+                Title = Input.Title,
+                Description = Input.Description
+            };
 
-            _context.Events.Add(Event);
+            _context.Events.Add(calendarEvent);
+            
+            if (Input.NotifyByEmail || Input.NotifyBySMS)
+            {
+                var notification = new Notification
+                {
+                    Event = calendarEvent,
+                    NotifyBySMS = Input.NotifyBySMS,
+                    NotifyByEmail = Input.NotifyByEmail,
+                    ScheduledFor = Input.ScheduledFor
+                };
+
+                _context.Notifications.Add(notification);
+            }
+            
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
