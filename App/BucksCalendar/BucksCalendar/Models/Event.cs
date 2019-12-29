5a71fac2bb3830242a3918aecb9631a4c82d172f
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-29 23:31:31 +0100

    [BC-006] Add AllDayEvent field for event model

--- App/BucksCalendar/BucksCalendar/Models/Event.cs
+++ App/BucksCalendar/BucksCalendar/Models/Event.cs
@@ -9,12 +9,12 @@ namespace BucksCalendar.Models
         public int EventID { get; set; }
         public string UserID { get; set; }
         public int CategoryID { get; set; }
+        public bool AllDayEvent { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public bool NotifyUsers { get; set; }
-        
+
         public Category Category { get; set; }
         public CalendarUser User { get; set; }
         public Notification Notification { get; set; }
