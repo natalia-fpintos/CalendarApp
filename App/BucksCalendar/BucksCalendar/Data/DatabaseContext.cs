17b8168d949b2fe93316863c245560dc405507da
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-31 20:40:39 +0100

    [BC-043] Seed database with test data and admin user

--- App/BucksCalendar/BucksCalendar/Data/DatabaseContext.cs
+++ App/BucksCalendar/BucksCalendar/Data/DatabaseContext.cs
@@ -57,6 +57,111 @@ namespace BucksCalendar.Data
                 RoleID = 3, Type = BucksCalendar.Models.Roles.Student, CanAddLecture = false, CanAddDeadline = false,
                 CanAddWFH = true, CanAddAnnualLeave = true, CanAddBankHolidays = false
             });
+            
+            // Seed Admin user
+            var userGuid = Guid.NewGuid().ToString();
+            modelBuilder.Entity<CalendarUser>().HasData(new CalendarUser
+            {
+                Id = userGuid,
+                UserName = "theadmin@admin.com",
+                NormalizedUserName = "THEADMIN@ADMIN.COM",
+                Email = "theadmin@admin.com",
+                NormalizedEmail = "THEADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAEAACcQAAAAEFrrxyboyIB1RkasF2XjHD9Oa0Z/Jo3tVqIH74oePSKZVJasx8dgKjGlRL+qwkRr2g==",
+                SecurityStamp = "AQAAAAEAACcQAAAAEFrrxyboyIB1RkasF2XjHD9Oa0Z/Jo3tVqIH74oePSKZVJasx8dgKjGlRL+qwkRr2g==",
+                ConcurrencyStamp = "e4232d05-6df2-459a-beb3-8b3d15a05faf",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                Name = "Admin",
+                Role = "Admin"
+            });
+            
+            // Seed test events
+            var events = new Event[]
+            {
+                new Event{
+                    EventID=1, UserID = userGuid, CategoryID=1, AllDayEvent=false, StartDateTime=DateTime.Parse("2019-12-06T10:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-06T18:00:00"), Title="Web Applications", 
+                    Description="In this lecture we will review the tutorial for Razor Pages with Entity Framework and will learn about database migrations.",
+                    Location="Uxbridge Campus - CS Lab"
+                },
+                new Event
+                {
+                    EventID=2, UserID = userGuid, CategoryID=1, AllDayEvent=false, StartDateTime=DateTime.Parse("2019-12-13T09:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-13T17:00:00"), Title="Web Applications",
+                    Description="A chance to work on our projects and ask any questions before the holidays.",
+                    Location="Uxbridge Campus - CS Lab"
+                },
+                new Event
+                {
+                    EventID=3, UserID = userGuid, CategoryID=2, AllDayEvent=false, StartDateTime=DateTime.Parse("2019-12-16T14:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-16T14:00:00"), Title="CW1-B (Logbook)",
+                    Description="Please ensure you review the assessment guide before you submit the document. Submit all logbooks as a single Word file."
+                },
+                new Event
+                {
+                    EventID=4, UserID = userGuid, CategoryID=3, AllDayEvent=true, StartDateTime=DateTime.Parse("2019-12-20T00:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-20T10:00:00"), Title="Self-study day"
+                },
+                new Event
+                {
+                    EventID=5, UserID = userGuid, CategoryID=3, AllDayEvent=false, StartDateTime=DateTime.Parse("2019-12-06T13:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-06T18:00:00"), Title="Natalia WFH"
+                },
+                new Event
+                {
+                    EventID=6, UserID = userGuid, CategoryID=4, AllDayEvent=true, StartDateTime=DateTime.Parse("2019-12-24T00:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-29T00:00:00"), Title="Natalia on AL",
+                    Description="Going home for Christmas!"
+                },
+                new Event
+                {
+                    EventID=7, UserID = userGuid, CategoryID=4, AllDayEvent=false, StartDateTime=DateTime.Parse("2019-12-12T16:30:00"),
+                    EndDateTime=DateTime.Parse("2019-12-13T13:00:00"), Title="Wafa on AL"
+                },
+                new Event
+                {
+                    EventID=8, UserID = userGuid, CategoryID=5, AllDayEvent=true, StartDateTime=DateTime.Parse("2019-12-25T00:00:00"),
+                    EndDateTime=DateTime.Parse("2019-12-25T00:00:00"), Title="Christmas Bank Holiday",
+                    Description="Merry Christmas!"
+                }
+            };
+            foreach (Event e in events)
+            {
+                modelBuilder.Entity<Event>().HasData(e);
+            }
+            
+            // Seed test notifications
+            var notifications = new Notification[]
+            {
+                new Notification
+                {
+                    NotificationID = 1, EventID = 1, NotifyBySMS = true, NotifyByEmail = true,
+                    ScheduledFor = DateTime.Parse("2019-12-05T10:00:00")
+                },
+                new Notification
+                {
+                    NotificationID = 2, EventID = 2, NotifyBySMS = true, NotifyByEmail = true,
+                    ScheduledFor = DateTime.Parse("2019-12-12T09:00:00")
+                },
+                new Notification
+                {
+                    NotificationID = 3, EventID = 3, NotifyBySMS = false, NotifyByEmail = true,
+                    ScheduledFor = DateTime.Parse("2019-12-13T14:00:00")
+                },
+                new Notification
+                {
+                    NotificationID = 4, EventID = 8, NotifyBySMS = true, NotifyByEmail = false,
+                    ScheduledFor = DateTime.Parse("2019-12-24T10:00:00")
+                }
+            };
+            foreach (Notification n in notifications)
+            {
+                modelBuilder.Entity<Notification>().HasData(n);
+            }
         }
     }
 }
