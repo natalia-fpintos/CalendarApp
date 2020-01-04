4ae6c719307d5b9f981a763842fc1e752d5afc8d
Natalia Fernández <natalia.fpintos@gmail.com>
2020-01-04 04:06:49 +0100

    [BC-018] Add designs for password update section

--- App/BucksCalendar/BucksCalendar/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ App/BucksCalendar/BucksCalendar/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -34,20 +34,20 @@ namespace BucksCalendar.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "This field is required.")]
             [DataType(DataType.Password)]
             [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "This field is required.")]
+            [StringLength(100, ErrorMessage = "Minimum {2} characters.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "Confirm password")]
+            [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
             public string ConfirmPassword { get; set; }
         }
 
