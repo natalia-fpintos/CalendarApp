bfb348dd5c77234f6dcd88bcb27505ff3d9ef36d
Natalia Fernández <natalia.fpintos@gmail.com>
2020-01-04 05:14:23 +0100

    [BC-000] Style profile pages with shared layout

--- App/BucksCalendar/BucksCalendar/wwwroot/js/site.js
+++ App/BucksCalendar/BucksCalendar/wwwroot/js/site.js
@@ -32,18 +32,6 @@ var mainProfile = $('main[role="main"]').has('div.profile-content');
 mainProfile.css('margin', '35px auto');
 mainProfile.css('width', '70%');
 
-// h2
-mainProfile.find("h2").css('color', '#ffffff');
-mainProfile.find("h2").css('font-family', '"Quicksand", sans-serif');
-
-// h4
-mainProfile.find("h4").css('color', '#ffffff');
-mainProfile.find("h4").css('font-family', '"Quicksand", sans-serif');
-mainProfile.find("h4").css('font-weight', 'lighter');
-
-// hr
-mainProfile.find("hr").css('border-color', 'transparent');
-
 // buttons
 mainProfile.find(".nav-pills .nav-link.active, .nav-pills .show > .nav-link").css('background-color', '#f85c6a');
 mainProfile.find(".nav-pills .nav-link").css('color', '#ffffff');
